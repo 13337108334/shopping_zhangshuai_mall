2bf057cf1df6cf6a44490e690efa0f7dcfb97d53
13337108334 <452101682@qq.com>
2024-08-24 15:22:15 +0800

    后端代码
    add

---
+++ online-mall-backend/src/main/java/com/shanzhu/em/mapper/FileMapper.java
@@ -0,0 +1,14 @@
+package com.shanzhu.em.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.shanzhu.em.entity.SysFile;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 用户上传文件 持久层（mapper）
+ *
+
+ */
+@Mapper
+public interface FileMapper extends BaseMapper<SysFile> {
+}
---
+++ online-mall-backend/src/main/java/com/shanzhu/em/mapper/IconCategoryMapper.java
@@ -0,0 +1,15 @@
+package com.shanzhu.em.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.shanzhu.em.entity.IconCategory;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 图片 - 分类关联持久层（mapper）
+ *
+
+ */
+@Mapper
+public interface IconCategoryMapper extends BaseMapper<IconCategory> {
+
+}
