99b0c6f57e1f2a08218433fbed13eafe4d3d6bbf
13337108334 <452101682@qq.com>
2024-08-24 13:04:36 +0800

    源代码
    push

---
+++ online-mall-backend/src/main/java/com/shanzhu/em/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.shanzhu.em.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.shanzhu.em.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 用户持久层 （mapper）
+ *
+ */
+@Mapper
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ online-mall-backend/src/main/java/com/shanzhu/em/utils/UserHolder.java
@@ -0,0 +1,25 @@
+package com.shanzhu.em.utils;
+
+import com.shanzhu.em.entity.User;
+
+/**
+ * 用户信息
+
+ */
+public class UserHolder {
+
+    private static final ThreadLocal<User> userThreadLocal = new ThreadLocal<>();
+
+    public static void saveUser(User user){
+        userThreadLocal.set(user);
+    }
+
+    public static User getUser(){
+        return userThreadLocal.get();
+    }
+
+    public static void removeUser(){
+        userThreadLocal.remove();
+    }
+
+}
